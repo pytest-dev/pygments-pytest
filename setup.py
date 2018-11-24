0a49d81ce0794e93fcc7a3bf2e3e8ae640207fc0
Anthony Sottile <asottile@umich.edu>
2018-11-23 17:54:46 -0800

    v0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pygments_pytest',
     description='A pygments lexer for pytest output.',
     url='https://github.com/asottile/pygments-pytest',
-    version='0.0.0',
+    version='0.0.1',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
